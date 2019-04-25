f82b8936e5f9fb43cd167969f53bc256b45beb91
TassioSense <tassio.jordao@sensenova.com.br>
2019-04-25 14:25:09 -0300

    separando em componentes

---
+++ src/componentes/Bar.vue
@@ -0,0 +1,43 @@
+<template>
+    
+    <div class="barra-externa">
+        <div class="barra-interna">
+         <p> {{ desempenho}} </p> 
+        </div>
+    </div>
+
+
+</template>
+
+<script>
+export default {
+
+    data() {
+		return {
+			desempenho: '10'
+		}
+	}
+
+}
+</script>
+
+<style>
+
+    .barra-externa {
+        display: flex;
+        justify-content: center;
+        width: 80%;
+        height: 20px;
+        border: 1px solid white;
+        border-radius: 3px;
+
+    }
+    .barra-externa .barra-interna  {
+        color: white;
+        display: flex;
+        justify-content: center;
+        text-align: center;
+        align-items: center
+    }
+
+</style>
